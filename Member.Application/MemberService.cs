3457cd1058ab12353cead1cd14fccc09c81c74a9
سامي اطنه لي <AD1008@iu.edu.jo>
2023-01-15 17:36:45 +0300

    Add project files.

---
+++ Member.Application/MemberService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Member.Application
+{
+    internal class MemberService : IMemberService
+    {
+        private readonly IMemberRepositry memberRepositry;
+        public MemberService(IMemberRepositry memberRepositry)
+        {
+            this.memberRepositry = memberRepositry;
+        }
+        List<Domain.Member> IMemberService.GetAllMembers()
+        {
+            return this.memberRepositry.GetAllMembers();
+        }
+    }
+}
